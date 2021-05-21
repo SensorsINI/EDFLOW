1cf23e6d1ebcf18c58c3cd030b4d426aa9f27004
Min Liu <wzygzlm@gmail.com>
2021-05-21 14:48:46 +0200

    Update abmofAccel.h

--- ABMOF/src/abmofAccel.h
+++ ABMOF/src/abmofAccel.h
@@ -91,9 +91,9 @@ const int glMinValidPixNumScale2 = int(glValidPixOccupancy * BLOCK_AREA_SCALE_2)
 // max allowed sad distance, normalized. Same as jaer.
 // To remove float computation on hardware, convert it back to int and only support weightDistance = 1.0
 const float maxAllowedSadDistance = 0.5;
-const int maxAllowedSadValueScale0 = ap_uint<BITS_PER_PIXEL>(0xffff) * 2 * BLOCK_AREA_SCALE_0;
-const int maxAllowedSadValueScale1 = ap_uint<BITS_PER_PIXEL>(0xffff) * 2 * BLOCK_AREA_SCALE_1;
-const int maxAllowedSadValueScale2 = ap_uint<BITS_PER_PIXEL>(0xffff) * 2 * BLOCK_AREA_SCALE_2;
+const int maxAllowedSadValueScale0 = ap_uint<BITS_PER_PIXEL>(0xffff) * 2 * BLOCK_AREA_SCALE_0 * maxAllowedSadDistance;
+const int maxAllowedSadValueScale1 = ap_uint<BITS_PER_PIXEL>(0xffff) * 2 * BLOCK_AREA_SCALE_1 * maxAllowedSadDistance;
+const int maxAllowedSadValueScale2 = ap_uint<BITS_PER_PIXEL>(0xffff) * 2 * BLOCK_AREA_SCALE_2 * maxAllowedSadDistance;
 
 #define BLOCK_COL_PIXELS BITS_PER_PIXEL * (BLOCK_SIZE + 2 * SEARCH_DISTANCE)
 #define BLOCK_SCALE0_COL_PIXELS BITS_PER_PIXEL * (BLOCK_SIZE_SCALE_0 + 2 * SEARCH_DISTANCE)
