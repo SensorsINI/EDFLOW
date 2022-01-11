17e8a32d3c78255e2e369443f551d409f6e6a159
Min Liu <wzygzlm@gmail.com>
2022-01-11 23:42:11 +0100

    Removed some intermidiate files.

--- EDFLOW_HLS_IPs/ABMOF/solution1/script.tcl
+++ EDFLOW_HLS_IPs/ABMOF/solution1/script.tcl
@@ -8,12 +8,12 @@ set_top EVABMOFStreamWithControl
 add_files topParseEvents/src/abmofAccel.h
 add_files topParseEvents/src/abmofAccel.cpp
 add_files -tb topParseEvents/src/test.cpp
-open_solution "solution1"
-set_part {xc7z007sclg225-1}
+open_solution "solution2"
+set_part {xc7z045ffg900-1}
 create_clock -period 10 -name default
 set_clock_uncertainty 0.1
 config_dataflow -default_channel fifo -fifo_depth 0
-source "./topParseEvents/solution1/directives.tcl"
+source "./topParseEvents/solution2/directives.tcl"
 csim_design -setup
 csynth_design
 cosim_design -trace_level all
